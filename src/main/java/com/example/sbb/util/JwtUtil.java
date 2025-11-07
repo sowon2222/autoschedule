92042e223909b79bae75d2a197544b9235550068
sowon2222 <js4848cc@gmail.com>
2025-11-07 17:33:42 +0900

    chore: remove unused map import in jwt util

--- src/main/java/com/example/sbb/util/JwtUtil.java
+++ src/main/java/com/example/sbb/util/JwtUtil.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
-import java.util.Map;
 
 @Component
 public class JwtUtil {
